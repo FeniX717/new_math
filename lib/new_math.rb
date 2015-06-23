25458ea6d78eddff9b9a2fd3383436d371392829
Kostya <konstantin.mikhaskov@gmail.com>
2015-06-23 14:47:09 +0300

    version 1.0.0

--- lib/new_math.rb
+++ lib/new_math.rb
@@ -4,7 +4,12 @@ require "new_mach_gem"
 module NewMath
   class MathPackage < NewMachGem::MathematicalOperations
   	def self.sin_and_cos
-  		'нога'
+  	  if(str=='sin')
+        result=Math.sin(x1)
+      elsif(str=='cos')
+        result=Math.cos(x1)
+      end
+      result
   	end
   end
 end
